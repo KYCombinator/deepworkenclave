4cb66102711d2e51591b2aa600db7e3823d8ef17
Dan Ross-Li <me@danrossli.com>
2024-12-07 22:21:26 -0500

    remove image loader file

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    loader: 'custom',
-    loaderFile: './image-loader.js',
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.squarespace-cdn.com",
-        port: "",
-        pathname: "/content/**",
-      }, //Squarespace
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      }, //Lorem Picsum
-    ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["*.execute-api.us-east-1.amazonaws.com"],
-    },
-  },
   output: "standalone", //for AWS SAM deployment
 };
 
