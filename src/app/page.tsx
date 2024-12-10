926cf45f2fac87c60e1e7f7b6496b3e0d9bf9693
Dan Ross-Li <me@danrossli.com>
2024-12-10 09:22:29 -0500

    add copy for deepwork enclave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Hero from '@/components/hero'
 
+export const metadata = {
+  openGraph: {
+    images: ['/DeepWorkEnclave.jpg'],
+    description: '4 startups, free rent, free office space, free food, for 1 year. No equity, No debt, no strings attached.',
+  },
+}
+
+
+
 export default function Home() {
   return <Hero />
 }
